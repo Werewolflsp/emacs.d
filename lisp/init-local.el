404417061bd88806c7fd0d695f5350adc49df954
Werewolflsp <werewolflsp@gmail.com>
2015-11-30 22:58:23 +0800

    php & html mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,5 +4,12 @@
 ;;; Code:
 (global-linum-mode t)
 
+(add-to-list 'auto-mode-alist '("\\.php\\'" . html-mode))
+
+(require 'mmm-auto)
+
+(setq mmm-global-mode 'maybe)
+(mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
+
 (provide 'init-local)
 ;;; init-local ends here
