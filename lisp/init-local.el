76f7d2787a0667de06d292feb005f72032120a4b
Werewolflsp <werewolflsp@gmail.com>
2015-12-08 23:49:36 +0800

    web-mode config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,12 +4,17 @@
 ;;; Code:
 (global-linum-mode t)
 
-(add-to-list 'auto-mode-alist '("\\.php\\'" . html-mode))
+;;; Web mode for php development
+(require-package 'web-mode)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 
-(require 'mmm-auto)
-
-(setq mmm-global-mode 'maybe)
-(mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+      )
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "wheat4")
+;;; End of web-mode config
 
 (provide 'init-local)
 ;;; init-local ends here
